26567cbbcde8d3aaeb402ab20ec4f14d00a442b0
NullAdmin123 <117506725+NullAdmin123@users.noreply.github.com>
2024-09-10 15:11:36 +0800

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,22 +1,29 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import axios from 'axios';
+import https from 'https';
 
-export default async function handler(
+export default function handler(
   request: VercelRequest,
   response: VercelResponse
 ) {
-  try {
-    const url = 'http://c4.ysepan.com/f_ht/ajcx/wj.aspx?cz=dq&jsq=0&mlbh=1938934&wjpx=1&_dlmc=xdtool&_dlmm=';
-    const result = await axios.get(url, {
-      headers: {
-        Referer: 'http://c4.ysepan.com/f_ht/ajcx/000ht.html?bbh=1172'
-      }
+  const url = 'https://c4.ysepan.com/f_ht/ajcx/wj.aspx?cz=dq&jsq=0&mlbh=1938934&wjpx=1&_dlmc=xdtool&_dlmm=';
+
+  https.get(url, {
+    headers: {
+      Referer: 'http://c4.ysepan.com/f_ht/ajcx/000ht.html?bbh=1172'
+    }
+  }, (res) => {
+    let data = '';
+
+    res.on('data', (chunk) => {
+      data += chunk;
     });
-    console.error(url);
-    console.error(result.data);
-    response.status(200).send(result.data);
-  } catch (error) {
-    console.error('Error fetching data:', error);
+
+    res.on('end', () => {
+      response.status(200).send(data);
+    });
+
+  }).on('error', (err) => {
+    console.error('Error fetching data:', err);
     response.status(500).send('请求失败');
-  }
+  });
 }
