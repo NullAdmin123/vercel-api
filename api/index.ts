be73a1a1d2866c96f1e8eaa5dd8116c425c1cc6d
NullAdmin123 <117506725+NullAdmin123@users.noreply.github.com>
2024-09-10 15:08:22 +0800

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,12 +1,20 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node'
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+import axios from 'axios';
 
-export default function handler(
+export default async function handler(
   request: VercelRequest,
   response: VercelResponse
 ) {
-  response.status(200).json({
-    body: request.body,
-    query: request.query,
-    cookies: request.cookies,
-  })
+  try {
+    const url = 'http://c4.ysepan.com/f_ht/ajcx/wj.aspx?cz=dq&jsq=0&mlbh=1938934&wjpx=1&_dlmc=xdtool&_dlmm=';
+    const result = await axios.get(url, {
+      headers: {
+        Referer: 'http://c4.ysepan.com/f_ht/ajcx/000ht.html?bbh=1172'
+      }
+    });
+    response.status(200).send(result.data);
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    response.status(500).send('请求失败');
+  }
 }
